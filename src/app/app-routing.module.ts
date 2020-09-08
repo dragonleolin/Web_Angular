ad49412be6df61c600bb89e38ae2ab4683e68dec
dragonleolin <wei.che0812@gmail.com>
2020-09-08 20:53:26 +0800

    ng init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  // { path: '', pathMatch: 'full', redirectTo: 'page' }, /**空白會跳轉到home */
+
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/',
+      }, /**空白會跳轉到home */
+      // { path: '/', component: HomeComponent },
+      //利用Service來存ID
+      // { path: 'youtubePage', component: YoutubePageComponent },
+      //利用網址列的方式來存ID
+      // { path: 'youtubePage/:id', component: YoutubePageComponent },
+      // { path: 'playerWidgets', component: PlayerWidgetsComponent },
+    ],
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/',
+  } /**萬用路由:隨便輸入都會到home */,
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
